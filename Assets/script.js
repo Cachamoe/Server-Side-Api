856de712e37ab9fd7a2af668b9c86bcc96a75360
Caleb Chaney <calebc347@gmail.com>
2020-09-11 13:34:19 -0500

    Updated HTML, CSS, and Javascript

--- Assets/script.js
+++ Assets/script.js
@@ -7,12 +7,11 @@ $("#searchButton").on("click", function (event) {
     event.preventDefault();
 
     var city = $("#weatherInput").val();
-
     Cities.push(city);
+    $("#fiveDay").show();
     createButtons();
 
-
-    // AJAX call to OpenWeatherMap API
+    // AJAX call to OpenWeatherMap API (current weather)
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey,
         method: "GET"
@@ -23,20 +22,47 @@ $("#searchButton").on("click", function (event) {
         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
         // Transfer to HTML
-        $(".city").html("<h1>" + response.name + moment().format('l') + "</h1>");
-        $(".tempF").text("Temperature (F): " + tempF);
-        $(".humidity").text("Humidity: " + response.main.humidity);
-        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".city").html("<h1>" + response.name + " " + "(" + moment().format('l') + ")" + "</h1>");
+        $(".tempF").text("Temperature: " + Math.round(tempF) + " °F");
+        $(".humidity").text("Humidity: " + response.main.humidity + "%");
+        $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");
         $(".uvIndex").text("UV Index: " + response.uvi);
 
-        //Console log results
+        // Console log results
         console.log("City: " + response.name);
-        console.log("Temperature (F): " + tempF);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Temperature: " + Math.round(tempF) + " °F");
+        console.log("Humidity: " + response.main.humidity + "%");
+        console.log("Wind Speed: " + response.wind.speed + " MPH");
         console.log("UV Index: " + response.uvi);
+
+        // AJAX call to OpenWeatherMap API (5-day forecast)
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            // Convert temp to fahrenheit
+            // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+            // Transfer to HTML
+            // $(".city").html("<h1>" + response.name + " " + "(" + moment().format('l') + ")" + "</h1>");
+            // $(".tempF").text("Temperature (F): " + tempF);
+            // $(".humidity").text("Humidity: " + response.main.humidity);
+            // $(".wind").text("Wind Speed: " + response.wind.speed);
+            // $(".uvIndex").text("UV Index: " + response.uvi);
+
+            // Console log results
+            // console.log("City: " + response.name);
+            // console.log("Temperature (F): " + tempF);
+            // console.log("Humidity: " + response.main.humidity);
+            // console.log("Wind Speed: " + response.wind.speed);
+            // console.log("UV Index: " + response.uvi);
+
+        });
     });
 });
+
 
 // Function for getting city name from data attr
 function getCityName() {
@@ -44,20 +70,21 @@ function getCityName() {
 }
 
 
-// Function for displaying city data
+// Function for creating buttons 
 function createButtons() {
     $("#weatherInput").val("");
+    $("#history").empty();
 
     for (var i = 0; i < Cities.length; i++) {
         var a = $("<button>");
         a.addClass("weather");
+        a.addClass("form-control");
         a.attr("data-name", Cities[i]);
         a.text(Cities[i]);
-        $("#search").append(a);
+        $("#history").append(a);
     }
 }
 
 
 $(document).on("click", ".weather", getCityName);
-
 
